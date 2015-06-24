d98f557633bd8da828bb3a016a60bd652b4180a4
mozillazg <opensource.mozillazg@gmail.com>
2015-06-24 10:22:57 +0800

    let `manage.py send` support the response that contains `attachments`

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import json
+from pprint import pprint
 
 from flask import current_app
 from flask_script import Manager, Server
@@ -23,10 +24,19 @@ def send(text):
     data['text'] = text
     rv = client.post(uri, data=data)
     if rv.status_code == 200:
-        if rv.data:
-            print(json.loads(rv.data)['text'])
+        body = rv.data
+        if not body:
+            print('Response body is empty!')
+            return
+
+        obj = json.loads(body)
+        if not obj.get('attachments'):
+            obj = obj['text']
+            print(obj)
+        else:
+            pprint(obj)
     else:
-        print('error!\nstatus code: %s\nbody: %s' % (rv.status_code, rv.data))
+        print('Error!\nstatus code: %s\nbody: %s' % (rv.status_code, rv.data))
 
 if __name__ == '__main__':
     manager.run()
